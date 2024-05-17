1fa953e4b2a061a346c98259ecd8a71ba2003d49
bhagyeshsp <bhagyeshspathak@gmail.com>
2024-05-17 20:38:19 +0530

    Major modifications in: before_actions, defined/redefined index, show, update, destroy

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,20 +1,20 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: %i[ show edit update destroy ]
-  before_action :set_teacher, only: [:new, :create]
-
+  before_action :set_teacher
+  before_action :set_student, only: [:edit, :update, :show, :destroy]
 
   # GET /students or /students.json
   def index
-    @students = Student.all
+    @student = Student.all
   end
 
   # GET /students/1 or /students/1.json
   def show
+    @student = Student.all
   end
 
   # GET /students/new
   #Modified to add teacher's id
-  def new 
+  def new
     @student = @teacher.students.build
   end
 
@@ -25,7 +25,7 @@ class StudentsController < ApplicationController
   # POST /students or /students.json
   #Modified to add teacher's id
   def create
-    @student = @teacher.students.build(student_params) 
+    @student = @teacher.students.build(student_params)
 
     respond_to do |format|
       if @student.save
@@ -42,7 +42,7 @@ class StudentsController < ApplicationController
   def update
     respond_to do |format|
       if @student.update(student_params)
-        format.html { redirect_to student_url(@student), notice: "Student was successfully updated." }
+        format.html { redirect_to @teacher, notice: "Student was successfully updated." }
         format.json { render :show, status: :ok, location: @student }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,25 +56,25 @@ class StudentsController < ApplicationController
     @student.destroy!
 
     respond_to do |format|
-      format.html { redirect_to students_url, notice: "Student was successfully destroyed." }
+      format.html { redirect_to teacher_student_url(@teacher), notice: "Student was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_student
-      @student = Student.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def student_params
-      params.require(:student).permit(:name, :gender, :grade, :attended, :teacher_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
-    # Created to find the teacher's id and pass it through the before_action event
-    def set_teacher
-      @teacher = Teacher.find(params[:teacher_id])
-    end
+  # Only allow a list of trusted parameters through.
+  def student_params
+    params.require(:student).permit(:name, :gender, :grade, :attended, :teacher_id)
+  end
 
+  # Created to find the teacher's id and pass it through the before_action event
+  def set_teacher
+    @teacher = Teacher.find(params[:teacher_id])
+  end
 end
